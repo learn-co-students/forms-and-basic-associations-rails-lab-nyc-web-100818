1a71abe124231baa06b678af3be0af287f675005
magic8baller <lturis@gmail.com>
2018-11-07 19:56:55 -0500

    first attempt-- unfinished

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,10 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
   # add associations here
+
+  def song_ids=(ids)
+    ids.each do |id|
+      song = Song.find_by(id)
+      self.songs << song
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,10 @@
 class Genre < ActiveRecord::Base
+  has_many :songs
   # add associations
+  def song_ids=(ids)
+    ids.each do |id|
+      song = Song.find_by(id)
+      self.songs << song
+    end
+  end
 end
